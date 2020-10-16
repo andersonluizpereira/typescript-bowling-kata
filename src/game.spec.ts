b3cf047dd6b370407e1838311beb45d1960da18c
Anderson Pereira <andersononus@hotmail.com>
2020-10-16 10:51:50 -0300

    test: extract function to make many rolls

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,6 +1,5 @@
 import { Game } from './game'
 
-
 let g: Game = new Game()
 
 beforeEach(() => {
@@ -9,19 +8,18 @@ beforeEach(() => {
 
 describe('Bowling Game', () => {
   test('Should score 0 for Gutter Game', () => {
-    const g: Game = new Game()
-    for(let i = 0; i < 20; i++) {
-      g.roll(0)
-    }
+    rollMany(20, 0)
     expect(g.score).toBe(0)
   })
 
   test('Should score 20 for all ones game', () => {
-    const g: Game = new Game()
-    for(let i = 0; i < 20; i++) {
-      g.roll(1)
-    }
+    rollMany(20, 0)
     expect(g.score).toBe(20)
   })
-  
-});
+
+  function rollMany (n: number, pins: number): void {
+    for (let i = 0; i < n; i++) {
+      g.roll(pins)
+    }
+  }
+})
