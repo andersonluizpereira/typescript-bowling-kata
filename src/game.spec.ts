96e6254640625b2efcef7125aa4c85209c9dd1c5
Anderson Pereira <andersononus@hotmail.com>
2020-10-16 10:41:19 -0300

    test: all one games

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -4,8 +4,17 @@ describe('Bowling Game', () => {
   test('Should score 0 for Gutter Game', () => {
     const g: Game = new Game()
     for(let i = 0; i < 20; i++) {
-      g.roll(i)
+      g.roll(0)
     }
-    expect(g.score()).toBe(0)
+    expect(g.score).toBe(0)
   })
+
+  test('Should score 20 for all ones game', () => {
+    const g: Game = new Game()
+    for(let i = 0; i < 20; i++) {
+      g.roll(1)
+    }
+    expect(g.score).toBe(20)
+  })
+  
 });
