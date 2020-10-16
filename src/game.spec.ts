de621bf5e451ce10a9c221a9ac14375c18d0f9f3
Anderson Pereira <andersononus@hotmail.com>
2020-10-16 11:19:24 -0300

    refactor: improve game design

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -9,13 +9,20 @@ beforeEach(() => {
 describe('Bowling Game', () => {
   test('Should score 0 for Gutter Game', () => {
     rollMany(20, 0)
-    expect(g.score).toBe(0)
+    expect(g.score()).toBe(0)
   })
 
   test('Should score 20 for all ones game', () => {
     rollMany(20, 1)
-    expect(g.score).toBe(20)
+    expect(g.score()).toBe(20)
   })
+
+  // test('should score 16 with a spare followed by a 3 ball', () => {
+  //   g.roll(5)
+  //   g.roll(5)
+  //   g.roll(3)
+  //   expect(g.score).toBe(16)
+  // })
 
   function rollMany (n: number, pins: number): void {
     for (let i = 0; i < n; i++) {
